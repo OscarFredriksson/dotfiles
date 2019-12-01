ecbab81c3498913e6b0a51beba9bbef2c369c185
Oscar Fredriksson <osfr1701@student.miun.se>
2019-12-01 02:54:32 +0100

    added script for creating symlinks

---
+++ scripts/add-symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+ln -s ~/.config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.config/zsh/zshrc ~/.zshrc  
+ln -s ~/.config/skhd/skhdrc ~/.skhdrc
+ln -s ~/.config/hyper/hyper.js ~/.hyper.js
+ln -s ~/.config/vim/vimrc ~/.vimrc
